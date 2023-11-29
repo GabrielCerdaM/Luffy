5f5e21623d34448e53f7a551731a4f8a70b344c9
GabrielCerdaM <gabriel.cerda.m@gmail.com>
2023-11-29 15:13:45 -0600

    User model added

--- target/generated-sources/annotations/com/mycompany/logic/Subcategory_.java
+++ target/generated-sources/annotations/com/mycompany/logic/Subcategory_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-28T23:00:32", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-29T14:33:16", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Subcategory.class)
 public class Subcategory_ { 
 
